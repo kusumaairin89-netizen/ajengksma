f8a95c0128bcadf9912cf75d358e05239c4079f1
kusumaairin89-netizen <kusumaairin89@gmail.com>
2025-10-21 13:29:25 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+//1. Pilih tombol berdasarkan ID-nya
+const tombolsapa=document.getElementById('sapabutton');
+//2. Tambahkan 'event listener' untuk aksi 'click'
+tombolsapa.aadEventlistener('click',function(){
+  //3. Jalankan kode ini ketika tombol di-klik
+                            elert('Halo! Terima kasih sudah berkunjung!');
+});
